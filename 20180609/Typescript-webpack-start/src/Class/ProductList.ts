7d16cbfe237cce239764677d68f610230a574d81
slk <slk@WSLK1GFTR4P-10.SLKSOFT.COM>
2018-06-09 16:47:40 +0530

    add jquery

--- 20180609/Typescript-webpack-start/src/Class/ProductList.ts
+++ 20180609/Typescript-webpack-start/src/Class/ProductList.ts
@@ -1,12 +1,15 @@
+import * as $ from 'jquery';
 import {IProduct} from '../Models/Product';
 import { Cart } from '../Class/Cart';
 export class ProductList {
         private prodListContainer: HTMLDivElement;
         constructor(public list: Array<IProduct>, public cart: Cart){
-            this.prodListContainer = document.querySelector(".product-list") as HTMLDivElement;
+            this.prodListContainer = document.querySelector('.product-list') as HTMLDivElement;
         }
         renderList(): void {
             this.list.forEach((item:IProduct) => {
+                const prodDivJqueryEl: JQuery<HTMLDivElement> = $('<div />') as JQuery<HTMLDivElement>;
+                
                 const divMain: HTMLDivElement = document.createElement('div');
                 divMain.className = 'product-item col-12, col-sm-6';
                 //divMain.className = "product-item";
@@ -25,6 +28,7 @@ export class ProductList {
                     this.cart.addToCart(item.id);
                 });
 
+                prodDivJqueryEl.append(prodTitleEl);
                 divMain.appendChild(divEl);
                 divMain.appendChild(imgEl);
                 divMain.appendChild(btnEl);
