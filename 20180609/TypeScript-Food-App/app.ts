1f5b3a361d7f84a34a45d8069f0ef3fa0d1bc724
slk <slk@WSLK1GFTR4P-10.SLKSOFT.COM>
2018-06-09 09:33:33 +0530

    Created items list

--- 20180609/TypeScript-Food-App/app.ts
+++ 20180609/TypeScript-Food-App/app.ts
@@ -32,10 +32,38 @@
 
     class ProductList{
         constructor(public list: Array<IProduct>){
-            
+
         }
         renderList(){
 
         }
     }
+    
+    const products: Array<IProduct> = [
+        {
+            id: 1,
+            title: "Full Meals",
+            imgURL: "~/images/FullMeals.jpg",
+            price: 50
+        },
+        {
+            id: 2,
+            title: "Poori",
+            imgURL: "~/images/Poori.jpg",
+            price: 30
+        },
+        {
+            id: 1,
+            title: "Samosa",
+            imgURL:"~/images/Poori.jpg",
+            price: 15
+        },
+        {
+            id: 1,
+            title: "Jamoon",
+            imgURL:"~/images/Jamoon.jpg",
+            price: 10
+        }
+    ];
+
 })()
