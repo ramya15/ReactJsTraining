cd9bdc826514dc57516183157cc9534c83aab664
slk <slk@WSLK1GFTR4P-10.SLKSOFT.COM>
2018-06-09 15:29:12 +0530

    spreadOperator

---
+++ 20180609/Typescript-Generics/app-spreadOperator.ts
@@ -0,0 +1,21 @@
+const a = {
+    myvar: 'anv',
+    another: ''
+};
+
+const b = {
+    ...a,   //spreadOperator
+    newprop:''
+};
+
+console.log(b);
+
+//JS Features
+// -this
+// -call
+// -apply
+// -bind
+
+// -Closure
+// -Higher Order Function
+// -Hoisting
