91bbf79709eb28dd19997cea397c6f7dc3059466
slk <slk@WSLK1GFTR4P-10.SLKSOFT.COM>
2018-06-11 10:04:13 +0530

    created new react app using ts

---
+++ 20180611/react-basic/src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        Hello
+        <button>Click Me!</button>  
+      </div>
+    );
+  }
+}
+
+export default App;
