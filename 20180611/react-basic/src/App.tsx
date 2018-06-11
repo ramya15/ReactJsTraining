57dd7b1ff5eb5dd951f7bc6b4279cbd59e09cfde
slk <slk@WSLK1GFTR4P-10.SLKSOFT.COM>
2018-06-11 11:41:12 +0530

    Query List

--- 20180611/react-basic/src/App.tsx
+++ 20180611/react-basic/src/App.tsx
@@ -10,10 +10,8 @@ interface IState {
   users: IUser[];
 }
 
-class App extends React.Component {
-  public state: Readonly<IState> = {
-      title: "Initial title",
-      users: [
+// Keeping original array
+const userList = [
       {
         name: "Jane"
       },
@@ -22,7 +20,12 @@ class App extends React.Component {
       },
       {
         name: "Dave"
-      }]
+      }];
+
+class App extends React.Component {
+  public state: Readonly<IState> = {
+      title: "Initial title",
+      users: userList
     }
 
   public btnClick = () => {
@@ -31,11 +34,28 @@ class App extends React.Component {
     });
   }
 
+  public onQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+    const filterUsers = userList.filter((u: IUser) => {
+      // const filterUsers = this.state.users.filter((u: IUser) => {
+      // For IE use: indexOf
+      return u.name.indexOf(inputValue) > -1;
+      // return u.name.includes(inputValue);
+    });
+
+    this.setState({
+      users: filterUsers
+    });
+  }
+
   public render() {
     const liItems = this.state.users.map((u,index) => (<li key={index}>{u.name}</li>));
     return (
       <div className="App">
-        {liItems}
+        <input type="text" onChange={this.onQuery}/>
+        <ul>
+          {liItems}
+        </ul>
       </div>
     );
   }
