8a1c50577c9d972476ea13bc87bc9d5cad918f57
slk <slk@WSLK1GFTR4P-10.SLKSOFT.COM>
2018-06-11 10:53:36 +0530

    button click

--- 20180611/react-basic/src/App.tsx
+++ 20180611/react-basic/src/App.tsx
@@ -2,11 +2,14 @@ import * as React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  public onBtnClick = () =>{
+      alert('Hello');
+    }
   public render() {
     return (
       <div className="App">
         Hello
-        <button>Click Me!</button>  
+        <button onClick={this.onBtnClick}>Click Me!</button>  
       </div>
     );
   }
